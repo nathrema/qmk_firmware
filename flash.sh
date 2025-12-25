396c774a514b19a844ff18f10d7ead81f5d8948c
user <user@banshee.lan>
2025-12-25 06:42:14 -0500

    update

--- flash.sh
+++ flash.sh
@@ -1,2 +1,5 @@
 cd keyboards/crkbd/keymaps/custom
-qmk flash -kb crkbd/rev4_1/standard -km custom
+qmk compile -kb crkbd/rev4_1/standard -km custom
+cd ../../../..
+sudo cp crkbd_rev4_1_standard_custom.uf2 /mnt
+rm crkbd_rev4_1_standard_custom.uf2
